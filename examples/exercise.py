32a49d82d6dd8e0c2a8201a182387837cecdaa91
Matt Plank <matthew_plank@outlook.com>
2025-10-11 13:49:07 +0100

    Refactored example

--- examples/exercise.py
+++ examples/exercise.py
@@ -1,14 +1,1 @@
-from dataclasses import dataclass
 
-
-@dataclass
-class Exercise:
-    id: int
-    name: str
-
-
-LIST = [
-    Exercise(0, "Bicep Curl"),
-    Exercise(1, "Bench Press"),
-    Exercise(2, "Lat Pulldown"),
-]
---
+++ examples/uvicorn.py
@@ -0,0 +1,10 @@
+from fragments import frag_loader
+
+frag_loader.init()
+
+import uvicorn
+
+from examples.fastapi import app
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
